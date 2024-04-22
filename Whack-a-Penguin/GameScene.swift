0f8106c872c86596eeddc9633e250e1ed6b454ab
Artsiom Chekh <qbartem@gmail.com>
2024-04-22 21:23:29 +0300

    initial commit

---
+++ Whack-a-Penguin/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Whack-a-Penguin
+//
+//  Created by avc on 22.04.24.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        <#code#>
+    }
+    
+}
